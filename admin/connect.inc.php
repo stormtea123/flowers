5afac2975a0e1acc6fb56965e2727b95754008eb
坦克 <stormte@gmail.com>
2015-01-28 03:16:48 +0800

    Update connect.inc.php

--- admin/connect.inc.php
+++ admin/connect.inc.php
@@ -1,5 +1,5 @@
 <?php
     include_once "ez_sql_core.php";  
     include_once "ez_sql_mysql.php"; 
-    $db = new ezSQL_mysql('ppms','ppms','ppms','localhost',$encoding = 'UTF-8')
+    $db = new ezSQL_mysql('ppms','name','password','localhost',$encoding = 'UTF-8')
 ?>
